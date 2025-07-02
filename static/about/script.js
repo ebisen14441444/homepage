3c5045c4547357b9b9dc916343ba3b365a882795
ebisen1444 <0294ryotaro@gmail.com>
2025-07-03 00:00:16 +0900

    背景の色が同時に変わるように

--- static/about/script.js
+++ static/about/script.js
@@ -1,6 +1,7 @@
 const text=document.querySelector('#colorText');
 const color = document.querySelector('#colorPicker');
 const colorBg =()=>{
+  document.body.style.backgroundColor = color.value;
   text.textContent=`カラーコード:${color.value}`;
 };
 
