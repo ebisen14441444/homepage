01daf35784ccf0a9b619b9a63aa7150fbed83153
ebisen1444 <0294ryotaro@gmail.com>
2025-07-02 23:06:44 +0900

    テンプレートリテラルを訂正

--- static/about/script.js
+++ static/about/script.js
@@ -1,7 +1,7 @@
 const text=document.querySelector('#colorText');
 const color = document.querySelector('#colorPicker');
 const colorBg =()=>{
-  text.textContent='カラーコード:${color.value}';
+  text.textContent=`カラーコード:${color.value}`;
 };
 
 color.addEventListener('input',colorBg);
