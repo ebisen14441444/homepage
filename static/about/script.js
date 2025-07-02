725b165f578cc6d68b497857629a7f565a8bc579
ebisen1444 <0294ryotaro@gmail.com>
2025-07-02 23:00:33 +0900

    カラーピッカーで選んだカラーのカラーコードが表示されるように

--- static/about/script.js
+++ static/about/script.js
@@ -1,6 +1,8 @@
 const text=document.querySelector('#colorText');
 const color = document.querySelector('#colorPicker');
+const colorBg =()=>{
+  text.textContent='カラーコード:${color.value}';
+};
 
-text.textContent=`カラーコード:${color.value}`;
 color.addEventListener('input',colorBg);
 
